6aa776ed40f9900e6d259889950e33e5a2dc18d2
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-03-05 20:12:47 -0600

    Add Environment Variables

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,95 +1,94 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const supabase = require('../supabaseClient');
+const supabase = require("../supabaseClient");
 
-router.get('/SignUp', (req, res) => {
-    res.render('SignUp');
-})
-
-router.post('/SignUp', async (req, res) => {
-    const {first_name, last_name, email, password} = req.body;
-
-    try {
-        const { user, session, error } = await supabase.auth.signUp(
-            {
-              email,
-              password,
-            },
-            {
-              data: { 
-                first_name, 
-                last_name,
-              }
-            }
-          )
-
-        if (error) throw error;
-
-        res.send('<script>alert("Check You Email For Verification")</script>')
-        res.redirect('/SignIn');
-    } catch (error) {
-        console.error(error);
-    }
-})
-
-router.get('/SignIn', (req, res) => {
-    const session = supabase.auth.session()
-    
-    session !== null ? res.redirect('/UserProfile') : res.render('SignIn');
-})
-
-router.post('/SignIn', async (req, res) => {
-    const { email, password } = req.body;
-    
-    try {
-        const { user, session, error } = await supabase.auth.signIn({
-            email,
-            password,
-        })
-
-        if (error) throw error;
-        
-        session !== null ? res.redirect('/UserProfile') : res.render('SignIn');
-    } catch (error) {
-        console.error(error);
-    }
-
-})
-
-router.get('/SignOut', async (req, res) => {
-    const { error } = await supabase.auth.signOut();
-    res.redirect('/SignIn');
-})
-
-router.get('/UserProfile', (req, res) => {
-    try {
-        const session = supabase.auth.session();
-        const userData = supabase.auth.user();
-
-        const emailJSON = JSON.stringify(userData.email);
-        const userJSON = JSON.stringify(userData.user_metadata.first_name + ' ' + userData.user_metadata.last_name);
-
-        const email = emailJSON.replace(/['"]+/g, '');
-        const user = userJSON.replace(/['"]+/g, '');
-
-        session !== null ? res.render('UserProfile', {email, user}) : res.redirect('/SignIn');
-    } catch (error) {
-        
-    }
+router.get("/SignUp", (req, res) => {
+  res.render("SignUp");
 });
 
-router.post('/UserProfile', async (req, res) => {
-    const { name, company, city, description } = req.body;
+router.post("/SignUp", async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
 
-    console.log(name, company, city, description);
+  try {
+    const { user, session, error } = await supabase.auth.signUp(
+      {
+        email,
+        password,
+      },
+      {
+        data: {
+          first_name,
+          last_name,
+        },
+      }
+    );
 
-    res.redirect('UserProfile');
-})
+    if (error) throw error;
+
+    res.send('<script>alert("Check You Email For Verification")</script>');
+    res.redirect("/SignIn");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/SignIn", (req, res) => {
+  const session = supabase.auth.session();
+
+  session !== null ? res.redirect("/UserProfile") : res.render("SignIn");
+});
+
+router.post("/SignIn", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const { user, session, error } = await supabase.auth.signIn({
+      email,
+      password,
+    });
+
+    if (error) throw error;
+
+    session !== null ? res.redirect("/UserProfile") : res.render("SignIn");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/SignOut", async (req, res) => {
+  const { error } = await supabase.auth.signOut();
+  res.redirect("/SignIn");
+});
+
+router.get("/UserProfile", (req, res) => {
+  try {
+    const session = supabase.auth.session();
+    const userData = supabase.auth.user();
+
+    const emailJSON = JSON.stringify(userData.email);
+    const userJSON = JSON.stringify(
+      userData.user_metadata.first_name + " " + userData.user_metadata.last_name
+    );
+
+    const email = emailJSON.replace(/['"]+/g, "");
+    const user = userJSON.replace(/['"]+/g, "");
+
+    session !== null
+      ? res.render("UserProfile", { email, user })
+      : res.redirect("/SignIn");
+  } catch (error) {}
+});
+
+router.post("/UserProfile", async (req, res) => {
+  const { name, company, city, description } = req.body;
+
+  console.log(name, company, city, description);
+
+  res.redirect("UserProfile");
+});
 
 async function getJobsForUser(user_id) {
-    let { data: jobs, error } = await supabase
-  .from('jobs')
-  .select('user_id')
+  let { data: jobs, error } = await supabase.from("jobs").select("user_id");
 }
 
 module.exports = router;
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,10 @@
-const { createClient } = require('@supabase/supabase-js')
+const { createClient } = require("@supabase/supabase-js");
 
-const API = {
-  base: 'https://jvdiaclesuxyipleqbqk.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2ZGlhY2xlc3V4eWlwbGVxYnFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ5NjY3MjYsImV4cCI6MTk2MDU0MjcyNn0.9O4Ba_q2JHxFGfpR2Lk2TzhJ6FI0xTGfD34kb4_54s8'
-}
+const NODE__ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+  path: `.env.${NODE__ENV}`,
+});
 
-const supabase = createClient(API.base, API.key)
+const supabase = createClient(process.env.BASE__SUPABASE, process.env.KEY__SUPABASE);
 
 module.exports = supabase;
